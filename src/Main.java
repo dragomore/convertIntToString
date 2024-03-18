47b2f48ad397849d90d580a5afca0cd6fc566d89
Miryoku <dragomore2804@gmail.com>
2024-03-18 17:36:15 +0200

    Rewrited array parsing logic

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
-import java.util.HashMap;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = { 1, };
+        int[] a = { 1,1,1,1,1,1,2,2,3,3 };
         convertArrayToString( a );
     }
-
+    public static void addWordKey(List<Map.Entry<Integer, Integer>> list, Integer key, Integer value){
+        list.add(new HashMap.SimpleEntry<>( key, value ));
+    }
     public static void convertArrayToString( int[] array ){
+        List<Map.Entry<Integer, Integer>> words = new ArrayList<>();
+
         char[] key1 = {'а', 'б', 'в', 'г'};
         char[] key2 = {'д', 'е', 'ё', 'ж'};
         char[] key3 = {'з', 'и', 'й', 'к'};
@@ -18,56 +22,91 @@ public class Main {
         char[] key9 = {'э', 'ю', 'я'};
 
         int tmp = array[0];
+        int tmpCount = 0;
         int count = 0;
-        HashMap<Integer, Integer> words = new HashMap<>();
 
         // get counts of nums
         for( int i = 0; i < array.length; i++ ){
             if( tmp != array[i] ) {
                 tmp = array[i];
+                addWordKey(words, tmp, count);
                 count = 0;
             }
+            if( count == 4 ){
+                addWordKey(words, tmp, 4);
+                count = 0;
+            }
+            if( i == array.length - 1 )
+                addWordKey(words, tmp, count + 1);
             count++;
-            words.put( tmp, count );
+//            System.out.println("i: " + i + " count: " + count);
         }
 
         StringBuilder convertedArray = new StringBuilder();
-        for( Integer i : words.keySet()){
-            switch( i ) {
+        for( Map.Entry<Integer, Integer> i : words){
+            System.out.println("Key: " + i.getKey() + " Value: " + i.getValue());
+            switch( i.getKey() ) {
                 case 1 : {
-                    convertedArray.append( key1[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key1[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key1[ i.getValue() ] );
                     break;
                 }
                 case 2 : {
-                    convertedArray.append( key2[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key2[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key2[ i.getValue() ] );
                     break;
                 }
                 case 3 : {
-                    convertedArray.append( key3[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key3[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key3[ i.getValue() ] );
                     break;
                 }
                 case 4 : {
-                    convertedArray.append( key4[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key4[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key4[ i.getValue() ] );
                     break;
                 }
                 case 5 : {
-                    convertedArray.append( key5[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key5[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key5[ i.getValue() ] );
                     break;
                 }
                 case 6 : {
-                    convertedArray.append( key6[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key6[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key6[ i.getValue() ] );
                     break;
                 }
                 case 7 : {
-                    convertedArray.append( key7[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key7[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key7[ i.getValue() ] );
                     break;
                 }
                 case 8 : {
-                    convertedArray.append( key8[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key8[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key8[ i.getValue() ] );
                     break;
                 }
                 case 9 : {
-                    convertedArray.append( key9[ words.get( i ) - 1 ] );
+                    if( i.getValue() != 0 )
+                        convertedArray.append( key9[ i.getValue() - 1 ] );
+                    else
+                        convertedArray.append( key9[ i.getValue() ] );
                     break;
                 }
             }
